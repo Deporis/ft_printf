08a32d902895280bb9b63ea673e47ea7d22d7a72
Alfred <alfred@Alfreds-MacBook-Pro.local>
2022-06-30 10:39:18 +0200

    Tests for pointer, how to convert decimal to hexa

--- src/ft_putpointer.c
+++ src/ft_putpointer.c
@@ -1,15 +1,31 @@
 #include "../lib/ft_printf.h"
 int	ft_putpointer(char *ptr)
 {
-	//char *ptr10;
-	//*ptr10 = ptr; //en base 10	
-	//printf("putpointer function %p == string %s\n", ptr, ptr10);
-	char n;
-	
-	n = 48;
-	if (ptr)
+	int i = 0;
+	while (ptr[i])
 	{
-		write(1, &n, 1);
+		printf("%c\n", (char)(&ptr)[i] - 20);
+		i++;
 	}
+	/*
+	//char *ptr10;
+	// *ptr10 = ptr; //en base 10	
+	//printf("putpointer function %p == string %s\n", ptr, ptr10);
+	size_t address = (size_t) ptr; // index + 2 [0, x, 1]
+	printf("adress of ptr? %zu\n", address);
+	int i = 0;
+	//char c;
+	write(1, "Is it equal to original? ", 26);
+	char c = address;
+	write(1, &c, 1);
+	write(1, "\n", 1);
+	
+	while (address[i])
+	{
+		c = 65; // *(address + i);
+		write(1, &c, 1);
+		i++;	
+	}
+	*/
 	return (10);
 }
