e47974abc699c9bf6512bd90f55c1a8d22a4483e
“ainga-ri” <alfred.inga.rios@gmail.com>
2022-07-04 18:08:10 +0200

    printf done waiting for paco

---
+++ src/ft_putupper.c
@@ -0,0 +1,19 @@
+#include "../lib/ft_printf.h"
+int	ft_putupper(unsigned long n)
+{
+	int len;
+	len = 0;
+	if (n >= 16)
+	{
+		return (ft_putupper(n / 16) + ft_putupper(n % 16));
+	}
+	else
+	{
+		if (n < 10)
+			ft_putchar(n + 48);
+		else
+			ft_putchar(n + 55);
+		len++;
+	}
+	return (len);
+}
