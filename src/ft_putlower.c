a2e23f68c315233162ba65f7c457bf3fc4aad540
Alfred <alfred@Alfreds-MacBook-Pro.local>
2022-07-08 18:44:07 +0200

    ALL DONE, only norminette left

--- src/ft_putlower.c
+++ src/ft_putlower.c
@@ -2,18 +2,34 @@
 int	ft_putlower(unsigned int n)
 {
 	int len;
+	int div;
+	int mod;
 
 	len = 0;
+	div = 0;
+	mod = 0;
 	if (n >= 16)
 	{
-		return (ft_putlower(n / 16) + ft_putlower(n % 16));
+		div = ft_putlower(n / 16);
+		if (div == -1)
+			return (-1);
+		mod = ft_putlower(n % 16);
+		if (mod == -1)
+			return (-1);
+		return (div + mod);
 	}
 	else
 	{
 		if (n < 10)
-			ft_putchar(n + 48);
+		{
+			if (ft_putchar(n + 48) == -1)
+				return (-1);
+		}
 		else
-			ft_putchar(n + 87);
+		{
+			if (ft_putchar(n + 87) == -1)
+				return (-1);
+		}
 		len++;
 	}
 	return (len);
